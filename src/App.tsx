d234fefd229e14fc514ea2d641b873d28b40f64f
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2021-11-16 14:46:28 -0300

    feat: responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,44 @@
+import housesImg from './assets/images/houses2.jpg';
+import logoDark from './assets/images/logo-dark.png';
+// import logoLight from './assets/images/logo-light.png';
+
 export default function App() {
-    return <div className="App text-xl">Helou World!</div>;
+    return (
+        <div className="App flex bg-gray-100">
+            <div className="p-8 max-w-md mx-auto sm:max-w-xl lg:py-24 lg:px-12 lg:max-w-full lg:w-1/2">
+                <div className="xl:max-w-lg xl:ml-auto">
+                    <img src={logoDark} alt="Homy logo" className="h-16" />
+                    <img
+                        src={housesImg}
+                        alt="Houses"
+                        className="mt-8 rounded-lg sm:h-64 sm:w-full  sm:object-cover sm:object-center lg:hidden"
+                    />
+                    <h1 className="mt-4 text-3xl font-bold text-gray-800 leading-tight tracking-tighter sm:text-4xl sm:mt-8">
+                        Your next home is waiting.
+                        <br />
+                        <span className="text-blue-800">Find it now!</span>
+                    </h1>
+                    <p className="mt-2 text-lg text-gray-500 sm:text-xl sm:mt-4">
+                        Homy helps you find your next nome, to rent or to be
+                        called your own!
+                    </p>
+                    <div className="mt-4 sm:mt-6">
+                        <a
+                            href="/"
+                            className="inline-block px-5  py-3 bg-blue-800 text-white uppercase font-semibold tracking-wider rounded-lg shadow-lg sm:text-lg"
+                        >
+                            See listings
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="hidden lg:block lg:w-1/2 lg:relative">
+                <img
+                    src={housesImg}
+                    alt="Houses"
+                    className="absolute inset-0 w-full h-full object-cover object-center"
+                />
+            </div>
+        </div>
+    );
 }
