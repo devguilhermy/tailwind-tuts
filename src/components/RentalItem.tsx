4dbfdaa15513498d7ba1d32bf78a959a8975f6a5
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2021-11-18 09:42:07 -0300

    feat: designed badges

--- src/components/RentalItem.tsx
+++ src/components/RentalItem.tsx
@@ -2,12 +2,14 @@ import { AiFillEye, AiFillStar } from 'react-icons/ai';
 
 interface RentalHouse {
     data: {
+        id: number;
         title: string;
         description: string;
         area: number;
         bedroom_count: number;
         bathroom_count: number;
         pictures: string[];
+        weekly_price: number;
         location: {
             address: string;
             latitude?: number;
@@ -15,17 +17,25 @@ interface RentalHouse {
         };
         rating: number;
         review_count: number;
+        is_new: boolean;
+        is_premium: boolean;
     };
 }
 
 export default function RentalItem({ data }: RentalHouse) {
     let stars: any = [];
-    for (let index = 1; index < data.rating; index++) {
-        stars.push(<AiFillStar />);
+    for (let index = 1; index <= 5; index++) {
+        stars.push(
+            <AiFillStar
+                className={
+                    index <= data.rating ? 'text-blue-600' : 'text-gray-300'
+                }
+            />
+        );
     }
 
     return (
-        <div className="w-full mx-auto max-w-md bg-white rounded-lg shadow-lg hover:shadow-xl transition">
+        <div className="w-full mx-auto max-w-md bg-white rounded-lg shadow-md hover:shadow-2xl transition">
             <div className="px-4 pt-4">
                 <img
                     src={data.pictures[0]}
@@ -34,26 +44,77 @@ export default function RentalItem({ data }: RentalHouse) {
                 />
             </div>
             <div className="p-4">
-                <p className="uppercase font-semibold text-sm text-gray-500 tracking-wide">
-                    <span>{data.bathroom_count} Baths </span>
-                    &bull;&nbsp;
-                    <span>{data.bedroom_count} Beds</span>
-                </p>
-                <h1 className="text-xl font-semibold">{data.title}</h1>
-                <p>{data.location.address}</p>
-                <div className="flex items-center justify-between">
-                    <div className="flex mt-2">
-                        <span className="flex text-xl text-blue-800 font-bold">
-                            {stars}
-                        </span>
-                        &nbsp;
-                        <span className="font-medium">{data.review_count}</span>
+                <div>
+                    <div className="flex items-center">
+                        <p>
+                            {data.is_new && (
+                                <span className="inline-block px-2  py-0.5 bg-blue-300 text-brand uppercase text-sm font-semibold tracking-wide rounded-full">
+                                    New
+                                </span>
+                            )}
+                            {data.is_premium && (
+                                <span
+                                    className={`${
+                                        data.is_new && 'ml-2'
+                                    } inline-block px-2 py-0.5 bg-yellow-300 text-yellow-800 uppercase text-sm font-semibold tracking-wide rounded-full`}
+                                >
+                                    Premium
+                                </span>
+                            )}
+                        </p>
+                        <p
+                            className={`${
+                                (data.is_new || data.is_premium) && 'ml-2'
+                            } uppercase font-semibold text-sm text-gray-500 tracking-wide`}
+                        >
+                            <span className="mr-1">
+                                {data.bathroom_count} Baths
+                            </span>
+                            &bull;
+                            <span className="mx-1">
+                                {data.bedroom_count} Beds
+                            </span>
+                            &bull;
+                            <span className="ml-1">{data.area} m²</span>
+                        </p>
                     </div>
-                    <button className="btn btn-primary mt-2 px-2 py-1 flex items-center justify-between">
-                        <AiFillEye className="mr-1" />
-                        Ver
-                    </button>
+                    <h1 className="mt-2 text-xl font-semibold leading-tight truncate">
+                        {data.title}
+                    </h1>
+                    <p className="mt-1 truncate">{data.location.address}</p>
                 </div>
+                <div className="flex items-center justify-between">
+                    <div className="">
+                        <span className="font-medium text-lg text-gray-600">
+                            $
+                        </span>
+                        <span className="text-2xl font-bold">
+                            {data.weekly_price}
+                        </span>
+                        <span className="ml-1 font-medium text-sm text-gray-600">
+                            {'/ wk'}
+                        </span>
+                        {/* <span className="ml-1 px-1 text-red-500  font-bold rounded-lg text-sm tracking-tighter uppercase">
+                            12% Off
+                        </span> */}
+                    </div>
+                    <div className="leading-3 text-right">
+                        <span className="flex items-center justify-items-stretch text-2xl text-brand-lighter font-bold">
+                            {stars}
+                            {/* <AiFillStar className="ml-auto" /> */}
+                            {/* <span>{data.rating}</span> */}
+                        </span>
+                        <span className="font-medium text-sm text-gray-600">
+                            {data.review_count} reviews
+                        </span>
+                    </div>
+                </div>
+                {/* <a
+                        href={`/rental/${data.id}`}
+                        className="btn btn-primary mt-2 px-2 py-1 flex items-center justify-center shadow-md"
+                    >
+                        <AiFillEye className="text-3xl" />
+                    </a> */}
             </div>
         </div>
     );
--- src/components/RentalList.tsx
+++ src/components/RentalList.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 export default function RentalList() {
     const [rentalList, setRentalList] = useState([
         {
+            id: 1,
             title: 'Nice modern Lake House',
             area: 420,
             bathroom_count: 5,
@@ -14,15 +15,19 @@ export default function RentalList() {
                 latitude: 56.232342,
                 longitude: -23.889424,
             },
+            weekly_price: 3800,
             pictures: [
                 'https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
                 'https://images.unsplash.com/photo-1621571113981-f51bde2a125b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80',
             ],
             rating: 4.5,
-            review_count: 78,
+            review_count: 18,
+            is_new: true,
+            is_premium: true,
         },
         {
-            title: 'Cottage in the mountains',
+            id: 2,
+            title: 'Cottage in the mountains in cold Switzerland',
             area: 129,
             bathroom_count: 3,
             bedroom_count: 4,
@@ -32,18 +37,23 @@ export default function RentalList() {
                 latitude: 46.561807,
                 longitude: 7.646569,
             },
+            weekly_price: 2800,
             pictures: [
                 'https://images.unsplash.com/photo-1567607351039-460841f82915?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80',
             ],
             rating: 5,
             review_count: 18,
+            is_new: false,
+            is_premium: true,
         },
         {
+            id: 3,
             title: 'Downtown modern house',
             area: 78,
             bathroom_count: 2,
             bedroom_count: 3,
             description: 'Technological house with aesthetic decoration.',
+            weekly_price: 1900,
             location: {
                 address: '12 Flowers Blvd, Chicago',
                 latitude: 41.933392,
@@ -54,18 +64,41 @@ export default function RentalList() {
             ],
             rating: 5,
             review_count: 33,
+            is_new: false,
+            is_premium: false,
+        },
+        {
+            id: 4,
+            title: 'Wooden house in the suburbs',
+            area: 104,
+            bathroom_count: 3,
+            bedroom_count: 4,
+            description: 'Technological house with aesthetic decoration.',
+            weekly_price: 1300,
+            location: {
+                address: '3 Marblehead, Massachusets',
+                latitude: 12.933392,
+                longitude: -37.646569,
+            },
+            pictures: [
+                'https://images.unsplash.com/photo-1570129477492-45c003edd2be?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80',
+            ],
+            rating: 3,
+            review_count: 12,
+            is_new: true,
+            is_premium: false,
         },
     ]);
 
     return (
-        <div className="mt-4 p-5">
+        <div className="mt-4 p-8 mx-auto">
             <div className="text-center sm:text-left">
                 <h1 className="text-3xl font-semibold">Premium rentals</h1>
                 <p>List of curated places for you to relax and enjoy.</p>
             </div>
             <div className="mt-4 grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                 {rentalList.map((rental) => {
-                    return <RentalItem data={rental} />;
+                    return <RentalItem data={rental} key={rental.id} />;
                 })}
             </div>
         </div>
