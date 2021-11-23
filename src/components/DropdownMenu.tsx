f0d2df9fed5189a7314311d629678f8d13f2656a
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2021-11-22 22:45:31 -0300

    feat: dropdown menu becomes nav list on mobile

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -5,13 +5,13 @@ interface DropdownMenuProps {
         url: string;
         name: string;
     }>;
-    className: string;
+    buttonClasses: string;
     children: ReactNode;
 }
 
 export default function DropdownMenu({
     links,
-    className,
+    buttonClasses,
     children,
 }: DropdownMenuProps) {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -40,7 +40,7 @@ export default function DropdownMenu({
     return (
         <div className="relative px-2 py-1 mt-6 sm:mt-0 sm:ml-2">
             <button
-                className={`relative z-30 ${className}`}
+                className={`relative z-30 ${buttonClasses}`}
                 onClick={() => setIsMenuOpen(!isMenuOpen)}
             >
                 {children}
@@ -56,14 +56,14 @@ export default function DropdownMenu({
             ></button>
             <div
                 className={`${
-                    isMenuOpen ? 'flex' : 'hidden'
-                } mt-2 py-2 w-48 static mx-auto sm:absolute sm:mx-0 z-30 right-0 flex-col  bg-white text-gray-800 text-lg sm:text-xl xl:text-2xl rounded-lg overflow-hidden shadow-lg`}
+                    isMenuOpen && 'sm:block'
+                } hidden mt-2 py-2 w-48 static mx-auto sm:absolute sm:mx-0 z-30 right-0 bg-white text-gray-800 text-lg sm:text-xl rounded-lg overflow-hidden shadow-lg`}
             >
                 {links.map((link, index) => {
                     return (
                         <a
                             href={`${link.url}`}
-                            className={`px-8 py-2 bg-white hover:bg-gray-600 hover:text-white focus:outline-none focus:bg-gray-600 focus:text-white active:bg-gray-700 `}
+                            className={`block px-8 py-2 bg-white hover:bg-gray-600 hover:text-white focus:outline-none focus:bg-gray-600 focus:text-white active:bg-gray-700 `}
                             key={index}
                         >
                             {link.name}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,9 +7,16 @@ import { useState } from 'react';
 export default function Header() {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
+    const dropdownLinks = [
+        { name: 'Account', url: '/account', icon: '' },
+        { name: 'My listings', url: '/my-listings', icon: '' },
+        { name: 'Support', url: '/support', icon: '' },
+        { name: 'Sign out', url: '/sign-out', icon: '' },
+    ];
+
     return (
-        <header className="z-10 px-4 py-3 bg-gray-900 sm:flex sm:items-center sm:justify-between ">
-            <div className="flex items-center justify-between">
+        <header className="z-10 bg-gray-900 sm:flex sm:items-center sm:justify-between ">
+            <div className="flex items-center justify-between px-4 py-3">
                 <div>
                     <img src={logoLight} alt="Homy logo" className="h-12" />
                 </div>
@@ -27,47 +34,71 @@ export default function Header() {
                     </button>
                 </div>
             </div>
-            <div
-                className={`pt-6 pb-4 px-1 text-white text-center transition text-3xl ${
+            <nav
+                className={`text-white transition  ${
                     isMobileMenuOpen ? 'block' : 'hidden'
-                } sm:flex sm:items-center sm:justify-between sm:text-xl`}
+                } sm:block`}
             >
-                <a
-                    href="/"
-                    className="block px-2 py-1 transition rounded-lg hover:bg-gray-700 focus:bg-gray-700 sm:mt-0 sm:rounded-full"
-                >
-                    Listings
-                </a>
-                <a
-                    href="/"
-                    className="block px-2 py-1 mt-6 transition rounded-lg hover:bg-gray-700 focus:bg-gray-700 sm:mt-0 sm:ml-2 sm:rounded-full"
-                >
-                    Add property
-                </a>
-                <a
-                    href="/"
-                    className="block px-2 py-1 mt-6 transition rounded-lg hover:bg-gray-700 focus:bg-gray-700 sm:mt-0 sm:ml-2 sm:rounded-full"
-                >
-                    Contact us
-                </a>
-                <DropdownMenu
-                    links={[
-                        { name: 'Account', url: '/account' },
-                        { name: 'Listings', url: '/my-listings' },
-                        { name: 'Support', url: '/support' },
-                        { name: 'Sign out', url: '/sign-out' },
-                    ]}
-                    className="w-10 h-10 overflow-hidden transition border-4 border-gray-600 rounded-full sm:block hover:border-gray-400 focus:outline-none focus:border-white"
-                >
-                    <img
-                        src={
-                            'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
-                        }
-                        alt="Avatar"
-                        className="object-cover object-center w-full h-full"
-                    />
-                </DropdownMenu>
-            </div>
+                <div className="px-4 py-3 text-2xl sm:flex sm:items-center sm:justify-between sm:text-xl">
+                    <a
+                        href="/"
+                        className="block px-2 py-1 transition rounded-lg hover:bg-gray-700 focus:bg-gray-700 sm:mt-0 sm:rounded-full"
+                    >
+                        Listings
+                    </a>
+                    <a
+                        href="/"
+                        className="block px-2 py-1 mt-6 transition rounded-lg hover:bg-gray-700 focus:bg-gray-700 sm:mt-0 sm:ml-2 sm:rounded-full"
+                    >
+                        Add property
+                    </a>
+                    <a
+                        href="/"
+                        className="block px-2 py-1 mt-6 transition rounded-lg hover:bg-gray-700 focus:bg-gray-700 sm:mt-0 sm:ml-2 sm:rounded-full"
+                    >
+                        Contact us
+                    </a>
+                    <DropdownMenu
+                        links={dropdownLinks}
+                        buttonClasses="hidden w-10 h-10 overflow-hidden transition border-4 border-gray-600 rounded-full sm:block hover:border-gray-400 focus:outline-none focus:border-white"
+                    >
+                        <img
+                            src={
+                                'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+                            }
+                            alt="Avatar"
+                            className="object-cover object-center w-full h-full"
+                        />
+                    </DropdownMenu>
+                </div>
+                <div className="px-6 py-6 border-t border-gray-800 sm:mt-0 sm:ml-2 sm:hidden">
+                    <div className={`w-full flex items-center `}>
+                        <img
+                            src={
+                                'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+                            }
+                            alt="Avatar"
+                            className="transition border-4 border-gray-600 rounded-full w-14 h-14 hover:border-gray-400 focus:outline-none focus:border-white cover-center"
+                        />
+                        <span className="ml-3 text-xl font-bold text-white">
+                            Marcus Aurelius
+                        </span>
+                    </div>
+                    <div className={`text-xl`}>
+                        {dropdownLinks.map((link, index) => {
+                            return (
+                                <a
+                                    href={`${link.url}`}
+                                    className={`mt-3 block text-gray-400 hover:text-white transition`}
+                                    key={index}
+                                >
+                                    {link.name}
+                                </a>
+                            );
+                        })}
+                    </div>
+                </div>
+            </nav>
         </header>
     );
 }
